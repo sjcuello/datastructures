4d82d2168c15c6ed5f8867af11175cc8d9df1201
sjcuello <santy.sc@hotmail.com>
2016-07-07 23:31:45 -0300

    Empezando con sorting

---
+++ sorting/Algorithms.java
@@ -0,0 +1,27 @@
+public class Algorithms{
+
+  public Algorithms{
+  }
+
+  public static void selectionSort(){
+
+  }
+
+  public static void bubbleSort(){
+
+  }
+
+  public static void insertionSort(){
+
+  }
+
+  public static void mergeSort(){
+
+  }
+
+  public static void quickSort(){
+
+  }
+
+
+}
